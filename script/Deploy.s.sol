f052c8b9203adeadefd40f2134ccac1ad0a309b7
mejango <mejango@protonmail.com>
2023-12-15 20:21:38 -0500

    deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,164 +4,91 @@ pragma solidity 0.8.23;
 import "lib/forge-std/src/Script.sol";
 
 import {IPermit2} from "lib/permit2/src/interfaces/IPermit2.sol";
-import "../src/JBPermissions.sol";
-import "../src/JBProjects.sol";
-import "../src/JBPrices.sol";
-import "../src/JBRulesets.sol";
-import "../src/JBDirectory.sol";
-import "../src/JBTokens.sol";
-import "../src/JBSplits.sol";
-import "../src/JBFeelessAddresses.sol";
-import "../src/JBFundAccessLimits.sol";
-import "../src/JBController.sol";
-import "../src/JBTerminalStore.sol";
-import "../src/JBMultiTerminal.sol";
+import "../src/JBProtocolDeployer.sol";
 
-contract Deploy is Script {
-    IPermit2 internal constant _PERMIT2 = IPermit2(0x000000000022D473030F116dDEE9F6B43aC78BA3);
+abstract contract Deploy is Script {
+    function _trustedForwarder() internal virtual returns (address);
+    function _manager() internal virtual returns (address);
 
-    JBPermissions _permissions;
-    JBProjects _projects;
-    JBPrices _prices;
-    JBDirectory _directory;
-    JBRulesets _rulesets;
-    JBTokens _tokens;
-    JBSplits _splits;
-    JBFeelessAddresses _feelessAddresses;
-    JBFundAccessLimits _fundAccessLimits;
-    JBController _controller;
-    JBTerminalStore _terminalStore;
-    JBMultiTerminal _multiTerminal;
-
-    function _run(address _manager, address _trustedForwarder) internal {
-        vm.startBroadcast();
-        _deployContracts(_manager, _trustedForwarder);
-        vm.stopBroadcast();
-    }
-
-    function _deployContracts(address _manager, address _trustedForwarder) internal {
-        _permissions = new JBPermissions();
-        _projects = new JBProjects(_manager);
-        _prices = new JBPrices(_permissions, _projects, _manager);
-        _feelessAddresses = new JBFeelessAddresses(_manager);
-        _directory = new JBDirectory(_permissions, _projects, msg.sender);
-        _splits = new JBSplits(_directory);
-        _fundAccessLimits = new JBFundAccessLimits(_directory);
-        _tokens = new JBTokens(_directory);
-        _rulesets = new JBRulesets(_directory);
-        _terminalStore = new JBTerminalStore(_directory, _rulesets, _prices);
-        _controller = new JBController(
-            _permissions, _projects, _directory, _rulesets, _tokens, _splits, _fundAccessLimits, _trustedForwarder
-        );
-        _directory.setIsAllowedToSetFirstController(address(_controller), true);
-        _directory.transferOwnership(_manager);
-        _multiTerminal = new JBMultiTerminal(
-            _permissions, _projects, _directory, _splits, _terminalStore, _feelessAddresses, _PERMIT2, _trustedForwarder
-        );
-    }
-
-    //https://ethereum.stackexchange.com/questions/24248/how-to-calculate-an-ethereum-contracts-address-during-its-creation-using-the-so
-    function addressFrom(address _origin, uint256 _nonce) internal pure returns (address _address) {
-        bytes memory data;
-        if (_nonce == 0x00) {
-            data = abi.encodePacked(bytes1(0xd6), bytes1(0x94), _origin, bytes1(0x80));
-        } else if (_nonce <= 0x7f) {
-            data = abi.encodePacked(bytes1(0xd6), bytes1(0x94), _origin, uint8(_nonce));
-        } else if (_nonce <= 0xff) {
-            data = abi.encodePacked(bytes1(0xd7), bytes1(0x94), _origin, bytes1(0x81), uint8(_nonce));
-        } else if (_nonce <= 0xffff) {
-            data = abi.encodePacked(bytes1(0xd8), bytes1(0x94), _origin, bytes1(0x82), uint16(_nonce));
-        } else if (_nonce <= 0xffffff) {
-            data = abi.encodePacked(bytes1(0xd9), bytes1(0x94), _origin, bytes1(0x83), uint24(_nonce));
-        } else {
-            data = abi.encodePacked(bytes1(0xda), bytes1(0x94), _origin, bytes1(0x84), uint32(_nonce));
-        }
-        bytes32 hash = keccak256(data);
-        assembly {
-            mstore(0, hash)
-            _address := mload(0)
-        }
+    function _run() internal {
+        vm.broadcast();
+        JBProtocolDeployer deployer = new JBProtocolDeployer();
+        vm.broadcast();
+        deployer.deployJBProtocol(_manager(), _trustedForwarder());
     }
 }
 
 // Ethereum
 contract DeployEthereumMainnet is Deploy {
-    address _trustedForwarder = 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
-    address _manager = 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
+    function _trustedForwarder() internal virtual override returns (address) {
+        return 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
+    }
 
-    function setUp() public {}
-
-    function run() public {
-        _run(_manager, _trustedForwarder);
+    function _manager() internal virtual override returns (address) {
+        return 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
     }
 }
 
 contract DeployEthereumGoerli is Deploy {
-    address _trustedForwarder = 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
-    address _manager = 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
+    function _trustedForwarder() internal virtual override returns (address) {
+        return 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
+    }
 
-    function setUp() public {}
-
-    function run() public {
-        _run(_manager, _trustedForwarder);
+    function _manager() internal virtual override returns (address) {
+        return 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
     }
 }
 
 contract DeployEthereumSepolia is Deploy {
-    address _trustedForwarder = 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
-    address _manager = 0x5a3aABEAAa3d7C7d86310dC769C494A9a5a730A1;
+    function _trustedForwarder() internal virtual override returns (address) {
+        return 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
+    }
 
-    function setUp() public {}
-
-    function run() public {
-        _run(_manager, _trustedForwarder);
+    function _manager() internal virtual override returns (address) {
+        return 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
     }
 }
 
 // Optimism
 
 contract DeployOptimismMainnet is Deploy {
-    address _trustedForwarder = 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
-    address _manager = 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
+    function _trustedForwarder() internal virtual override returns (address) {
+        return 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
+    }
 
-    function setUp() public {}
-
-    function run() public {
-        _run(_manager, _trustedForwarder);
+    function _manager() internal virtual override returns (address) {
+        return 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
     }
 }
 
 contract DeployOptimismTestnet is Deploy {
-    address _trustedForwarder = 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
-    address _manager = 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
+    function _trustedForwarder() internal virtual override returns (address) {
+        return 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
+    }
 
-    function setUp() public {}
-
-    function run() public {
-        _run(_manager, _trustedForwarder);
+    function _manager() internal virtual override returns (address) {
+        return 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
     }
 }
 
 // Polygon
 
 contract DeployPolygonMainnet is Deploy {
-    address _trustedForwarder = 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
-    address _manager = 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
+    function _trustedForwarder() internal virtual override returns (address) {
+        return 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
+    }
 
-    function setUp() public {}
-
-    function run() public {
-        _run(_manager, _trustedForwarder);
+    function _manager() internal virtual override returns (address) {
+        return 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
     }
 }
 
 contract DeployPolygonMumbai is Deploy {
-    address _trustedForwarder = 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
-    address _manager = 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
+    function _trustedForwarder() internal virtual override returns (address) {
+        return 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
+    }
 
-    function setUp() public {}
-
-    function run() public {
-        _run(_manager, _trustedForwarder);
+    function _manager() internal virtual override returns (address) {
+        return 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
     }
 }
---
+++ src/JBProtocolDeployer.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {IPermit2} from "lib/permit2/src/interfaces/IPermit2.sol";
+import "./JBPermissions.sol";
+import "./JBProjects.sol";
+import "./JBPrices.sol";
+import "./JBRulesets.sol";
+import "./JBDirectory.sol";
+import "./JBTokens.sol";
+import "./JBSplits.sol";
+import "./JBFeelessAddresses.sol";
+import "./JBFundAccessLimits.sol";
+import "./JBController.sol";
+import "./JBTerminalStore.sol";
+import "./JBMultiTerminal.sol";
+
+contract JBProtocolDeployer {
+    IPermit2 internal constant _PERMIT2 = IPermit2(0x000000000022D473030F116dDEE9F6B43aC78BA3);
+
+    constructor() {}
+
+    /// @notice Deploys the protocol.
+    /// @param manager The address that will manage the few privileged functions of the protocol.
+    /// @param trustedForwarder The address that is allowed to forward calls to the terminal and controller on a payer's
+    /// behalf.
+    function deployJBProtocol(address manager, address trustedForwarder) external {
+        JBPermissions permissions = new JBPermissions();
+        JBProjects projects = new JBProjects(manager);
+        JBDirectory directory = new JBDirectory(permissions, projects, msg.sender);
+        JBSplits splits = new JBSplits(directory);
+        JBRulesets rulesets = new JBRulesets(directory);
+        directory.setIsAllowedToSetFirstController(
+            address(
+                new JBController({
+                    permissions: permissions,
+                    projects: projects,
+                    directory: directory,
+                    rulesets: rulesets,
+                    tokens: new JBTokens(directory),
+                    splits: splits,
+                    fundAccessLimits: new JBFundAccessLimits(directory),
+                    trustedForwarder: trustedForwarder
+                })
+            ),
+            true
+        );
+        directory.transferOwnership(manager);
+        new JBMultiTerminal({
+            permissions: permissions,
+            projects: projects,
+            directory: directory,
+            splits: splits,
+            store: new JBTerminalStore({
+                directory: directory,
+                rulesets: rulesets,
+                prices: new JBPrices(permissions, projects, manager)
+            }),
+            feelessAddresses: new JBFeelessAddresses(manager),
+            permit2: _PERMIT2,
+            trustedForwarder: trustedForwarder
+        });
+    }
+}
