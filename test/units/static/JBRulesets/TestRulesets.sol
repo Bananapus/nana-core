d29eda5db8bfeac1827e9ef216c9c15e89cc82ca
nowonder <justinpulley92@gmail.com>
2023-12-18 17:04:51 +0000

    expect event, further checks, happy path queueFor.

--- test/units/static/JBRulesets/TestRulesets.sol
+++ test/units/static/JBRulesets/TestRulesets.sol
@@ -1,14 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import {Test} from 'forge-std/Test.sol';
-
 import /* {*} from */ "../../../helpers/TestBaseWorkflow.sol";
 
 /**
  * @title 
  */
 contract TestJBRulesetsUnits_Local is Test {
+
+    // Events
+    event RulesetQueued(
+        uint256 indexed rulesetId,
+        uint256 indexed projectId,
+        uint256 duration,
+        uint256 weight,
+        uint256 decayRate,
+        IJBRulesetApprovalHook hook,
+        uint256 metadata,
+        uint256 mustStartAtOrAfter,
+        address caller
+    );
+
+    event RulesetInitialized(uint256 indexed rulesetId, uint256 indexed projectId, uint256 indexed basedOnId);
+
     // Contracts
     JBRulesets public _rulesets;
     IJBDirectory internal _directory;
@@ -17,6 +31,45 @@ contract TestJBRulesetsUnits_Local is Test {
     // Necessary params
     JBRulesetMetadata private _metadata;
     uint256 _packedMetadata;
+    uint256 _projectId = 1;
+    uint256 _duration = 14;
+    uint256 _weight = 0;
+    uint256 _decayRate = 450_000_000;
+    uint256 _mustStartAt = 0;
+    IJBRulesetApprovalHook private _hook = IJBRulesetApprovalHook(address(0));
+
+
+    function equals(JBRuleset memory queued, JBRuleset memory stored) internal pure returns (bool) {
+        // Just compare the output of hashing all fields packed.
+        return (
+            keccak256(
+                abi.encodePacked(
+                    queued.cycleNumber,
+                    queued.id,
+                    queued.basedOnId,
+                    queued.start,
+                    queued.duration,
+                    queued.weight,
+                    queued.decayRate,
+                    queued.approvalHook,
+                    queued.metadata
+                )
+            )
+                == keccak256(
+                    abi.encodePacked(
+                        stored.cycleNumber,
+                        stored.id,
+                        stored.basedOnId,
+                        stored.start,
+                        stored.duration,
+                        stored.weight,
+                        stored.decayRate,
+                        stored.approvalHook,
+                        stored.metadata
+                    )
+                )
+        );
+    }
 
     function setUp() public {
 
@@ -51,27 +104,49 @@ contract TestJBRulesetsUnits_Local is Test {
 
     }
 
-    function testQueueFor() public {
-        // Setup: queueFor will call onlyControllerOf -> Directory to see if caller has proper permissions, mock that.
+    function testQueueForHappyPath() public {
+        // Setup: queueFor will call onlyControllerOf modifier -> Directory.controllerOf to see if caller has proper permissions, mock that.
         vm.mockCall(address(_directory), abi.encodeCall(IJBDirectory.controllerOf, (1)), abi.encode(address(this)));
 
-        // Check: Ensure calls go through
+        // Setup: Ensure calls go through
         vm.expectCall(
             address(_directory), abi.encodeCall(IJBDirectory.controllerOf, (1))
         );
 
+        // Setup: expect ruleset event (RulesetQueued) is emitted
+        vm.expectEmit();
+        emit RulesetQueued(block.timestamp, _projectId, _duration, _weight , _decayRate, _hook, _packedMetadata, block.timestamp, address(this));
+
         // Send: Call from this contract as it's been mock authorized above.
         _rulesets.queueFor({
-            projectId: 1,
-            duration: 14,
-            weight: 0,
-            decayRate: 450_000_000,
-            approvalHook: IJBRulesetApprovalHook(address(0)),
+            projectId: _projectId,
+            duration: _duration,
+            weight: _weight,
+            decayRate: _decayRate,
+            approvalHook: _hook,
             metadata: _packedMetadata,
-            mustStartAtOrAfter: 0
+            mustStartAtOrAfter: _mustStartAt
         });
 
-        // To-do: check ruleset was properly set & event (RulesetQueued) was emitted
+        // Get a reference to the now configured ruleset.
+        JBRuleset memory configuredRuleset = _rulesets.currentOf(_projectId);
+
+        // Reference queued attributes for sake of comparison.
+        JBRuleset memory queued = JBRuleset({
+            cycleNumber: 1,
+            id: block.timestamp,
+            basedOnId: 0,
+            start: block.timestamp,
+            duration: _duration,
+            weight: _weight,
+            decayRate: _decayRate,
+            approvalHook: _hook,
+            metadata: configuredRuleset.metadata
+        });
+
+        // Check: structs are the same.
+        bool same = equals(queued, configuredRuleset);
+        assertEq(same, true);
     }
 
 }
