e720f22d9ad8b56189a36e878ca830d7d9ecd2bc
nowonder <justinpulley92@gmail.com>
2024-01-18 20:38:01 +0000

    setMetadataOf cov

---
+++ test/units/static/JBController/TestSetMetadataOf.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import /* {*} from */ "../../../helpers/TestBaseWorkflow.sol";
+import {JBControllerSetup} from "./JBControllerSetup.sol";
+
+contract TestSetMetadataOf_Local is JBTest, JBControllerSetup {
+    function setUp() public {
+        super.controllerSetup();
+    }
+
+    function test_WhenCallerIsOwnerOrHasPermission() external {
+        // it should set metadata and emit SetMetadata
+        // mock ownerOf call
+        bytes memory _ownerOfCall = abi.encodeCall(IERC721.ownerOf, (1));
+        bytes memory _ownerData = abi.encode(address(this));
+
+        mockExpect(address(projects), _ownerOfCall, _ownerData);
+
+        vm.expectEmit();
+        emit IJBController.SetMetadata(1, "Juicay", address(this));
+
+        _controller.setMetadataOf(1, "Juicay");
+    }
+
+    function test_RevertWhen_CallerIsNotOwnerOrHasPermission() external {
+        // it should revert
+        // mock ownerOf call
+        bytes memory _ownerOfCall = abi.encodeCall(IERC721.ownerOf, (1));
+        bytes memory _ownerData = abi.encode(address(1));
+
+        mockExpect(address(projects), _ownerOfCall, _ownerData);
+
+        // mock first permissions call
+        bytes memory _permissionsCall = abi.encodeCall(
+            IJBPermissions.hasPermission, (address(this), address(1), 1, JBPermissionIds.SET_PROJECT_METADATA)
+        );
+        bytes memory _permissionsReturned = abi.encode(false);
+
+        mockExpect(address(permissions), _permissionsCall, _permissionsReturned);
+
+        // mock second permissions call
+        bytes memory _permissionsCall2 = abi.encodeCall(
+            IJBPermissions.hasPermission, (address(this), address(1), 0, JBPermissionIds.SET_PROJECT_METADATA)
+        );
+        bytes memory _permissionsReturned2 = abi.encode(false);
+
+        mockExpect(address(permissions), _permissionsCall2, _permissionsReturned2);
+
+        vm.expectRevert(abi.encodeWithSignature("UNAUTHORIZED()"));
+        _controller.setMetadataOf(1, "Not Juicay");
+    }
+}
