8887f22d0eb793e48a8739972d56ee791e57bf17
nowonder <justinpulley92@gmail.com>
2024-01-20 03:39:18 +0000

    setPrimaryTerminalOf cov

--- test/units/static/JBDirectory/TestSetPrimaryTerminalOf.sol
+++ test/units/static/JBDirectory/TestSetPrimaryTerminalOf.sol
@@ -20,10 +20,6 @@ contract TestSetPrimaryTerminalOf_Local is JBTest, JBDirectorySetup {
         _;
     }
 
-    modifier givenThatTheTerminalHasBeenAdded() {
-        _;
-    }
-
     modifier whenCallerHasPermission() {
         // mock ownerOf call
         bytes memory _ownerOfCall = abi.encodeCall(IERC721.ownerOf, (1));
@@ -45,40 +41,54 @@ contract TestSetPrimaryTerminalOf_Local is JBTest, JBDirectorySetup {
         _;
     }
 
-    /* function test_WhenCallerHasNoPermission() external {
+    function test_WhenCallerHasNoMaidens() external {
         // it should revert with UNAUTHORIZED()
+        // mock ownerOf call
+        bytes memory _ownerOfCall = abi.encodeCall(IERC721.ownerOf, (1));
+        bytes memory _ownerData = abi.encode(address(1));
+
+        mockExpect(address(projects), _ownerOfCall, _ownerData);
+
+        // mock first permissions call
+        bytes memory _permissionsCall = abi.encodeCall(
+            IJBPermissions.hasPermission, (address(this), address(1), 1, JBPermissionIds.SET_PRIMARY_TERMINAL)
+        );
+        bytes memory _permissionsReturned = abi.encode(false);
+
+        mockExpect(address(permissions), _permissionsCall, _permissionsReturned);
+
+        // mock second permissions call
+        bytes memory _permissionsCall2 = abi.encodeCall(
+            IJBPermissions.hasPermission, (address(this), address(1), 0, JBPermissionIds.SET_PRIMARY_TERMINAL)
+        );
+        bytes memory _permissionsReturned2 = abi.encode(false);
+
+        mockExpect(address(permissions), _permissionsCall2, _permissionsReturned2);
+
+        vm.expectRevert(abi.encodeWithSignature("UNAUTHORIZED()"));
+        _directory.setPrimaryTerminalOf(1, _token, _terminalToAdd);
     }
 
     function test_GivenThatThereIsNoAccountingContextForTokenOf() external whenCallerHasPermission {
         // it should revert with TOKEN_NOT_ACCEPTED
+        // accounting context
+        JBAccountingContext memory _context = JBAccountingContext({token: address(0), decimals: 6, currency: uint32(1)});
+
+        // mock accountingContext call
+        bytes memory _contextCall = abi.encodeCall(IJBTerminal.accountingContextForTokenOf, (1, _token));
+        bytes memory _contextReturn = abi.encode(_context);
+
+        mockExpect(address(_terminalToAdd), _contextCall, _contextReturn);
+
+        vm.expectRevert(abi.encodeWithSignature("TOKEN_NOT_ACCEPTED()"));
+        _directory.setPrimaryTerminalOf(1, _token, _terminalToAdd);
     }
 
-    function test_GivenThatTheTerminalHasAlreadyBeenAdded()
+    function testFail_GivenThatTheTerminalHasAlreadyBeenAdded()
         external
         whenCallerHasPermission
         givenThatThereIsAnAccountingContextForTokenOf
     {
-        // it should not add the terminal
-    }
-
-    function test_GivenThatTheProjectIsNotAllowedToSetTerminals()
-        external
-        whenCallerHasPermission
-        givenThatThereIsAnAccountingContextForTokenOf
-        givenThatTheTerminalHasNotBeenAdded
-    {
-        // it should revert with SET_TERMINALS_NOT_ALLOWED
-    } */
-
-    function test_GivenThatTheProjectIsAllowedToSetTerminals()
-        external
-        whenCallerHasPermission
-        givenThatThereIsAnAccountingContextForTokenOf
-        givenThatTheTerminalHasNotBeenAdded
-    {
-        // it should set the terminal and emit AddTerminal
-        // it should set the terminal as primary and emit SetPrimaryTerminal
-
         address _mockController = makeAddr("controller");
 
         stdstore.target(address(_directory)).sig("controllerOf(uint256)").with_key(1).depth(0).checked_write(
@@ -91,7 +101,72 @@ contract TestSetPrimaryTerminalOf_Local is JBTest, JBDirectorySetup {
 
         mockExpect(address(_mockController), _supportCall, _supportReturned);
 
-        /* vm.prank(_mockController); */
+        // it should not add the terminal, but set it as primary
+        IJBTerminal[] memory _terminals = new IJBTerminal[](1);
+        _terminals[0] = _terminalToAdd;
+
+        vm.expectEmit();
+        emit IJBDirectory.SetTerminals(1, _terminals, address(this));
+
+        _directory.setTerminalsOf(1, _terminals);
+
+        // prove that the terminal was not added - forces this test to fail as intended
+        vm.expectEmit();
+        emit IJBDirectory.AddTerminal(1, _terminals[0], address(this));
+
+        // set the primary next and mock whatever calls
+        _directory.setPrimaryTerminalOf(1, _token, _terminals[0]);
+    }
+
+    function test_GivenThatTheProjectIsNotAllowedToSetTerminals()
+        external
+        whenCallerHasPermission
+        givenThatThereIsAnAccountingContextForTokenOf
+    {
+        // it should revert with SET_TERMINALS_NOT_ALLOWED
+        address _mockController = makeAddr("controller");
+
+        stdstore.target(address(_directory)).sig("controllerOf(uint256)").with_key(1).depth(0).checked_write(
+            _mockController
+        );
+
+        // mock erc165 call
+        bytes memory _supportCall =
+            abi.encodeCall(IERC165.supportsInterface, (type(IJBDirectoryAccessControl).interfaceId));
+        bytes memory _supportReturned = abi.encode(true);
+
+        mockExpect(address(_mockController), _supportCall, _supportReturned);
+
+        // mock setTerminalsAllowed call
+        bytes memory _allowedCall = abi.encodeCall(IJBDirectoryAccessControl.setTerminalsAllowed, (1));
+        bytes memory _allowedReturn = abi.encode(false);
+
+        mockExpect(address(_mockController), _allowedCall, _allowedReturn);
+
+        vm.expectRevert(abi.encodeWithSignature("SET_TERMINALS_NOT_ALLOWED()"));
+        _directory.setPrimaryTerminalOf(1, _token, _terminalToAdd);
+    }
+
+    function test_GivenThatTheProjectIsAllowedToSetTerminals()
+        external
+        whenCallerHasPermission
+        givenThatThereIsAnAccountingContextForTokenOf
+        givenThatTheTerminalHasNotBeenAdded
+    {
+        // it should set the terminal and emit AddTerminal
+        // it should set the terminal as primary and emit SetPrimaryTerminal
+        address _mockController = makeAddr("controller");
+
+        stdstore.target(address(_directory)).sig("controllerOf(uint256)").with_key(1).depth(0).checked_write(
+            _mockController
+        );
+        // mock erc165 call
+        bytes memory _supportCall =
+            abi.encodeCall(IERC165.supportsInterface, (type(IJBDirectoryAccessControl).interfaceId));
+        bytes memory _supportReturned = abi.encode(false);
+
+        mockExpect(address(_mockController), _supportCall, _supportReturned);
+
         _directory.setPrimaryTerminalOf(1, _token, _terminalToAdd);
     }
 }
