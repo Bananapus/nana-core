91b3b0cd4577c36a3eb009e23f976edb4395ebea
nowonder <justinpulley92@gmail.com>
2024-01-27 14:45:13 +0000

    feelessAddresses cov

--- test/units/static/JBFeelessAdresses/TestInterfaces.sol
+++ test/units/static/JBFeelessAdresses/TestInterfaces.sol
@@ -11,9 +11,19 @@ contract TestSupportsInterface_Local is JBFeelessSetup {
 
     function test_WhenItSupportsEitherIJBFeelessAddressesOrIERC165() external {
         // it should return true
+        bool result1 = IERC165(address(_feelessAddresses)).supportsInterface(type(IJBFeelessAddresses).interfaceId);
+        assertEq(result1, true);
+
+        bool result2 = IERC165(address(_feelessAddresses)).supportsInterface(type(IERC165).interfaceId);
+        assertEq(result2, true);
     }
 
-    function test_WhenItDoesntSupportEitherIJBFeelessAddressesOrIERC165() external {
+    function test_WhenAskedIfSupportsNonIJBFeelessAddressesOrIERC165() external {
         // it should return false
+        bool result1 = IERC165(address(_feelessAddresses)).supportsInterface(0x12345678);
+        assertEq(result1, false);
+
+        bool result2 = IERC165(address(_feelessAddresses)).supportsInterface(0x12345679);
+        assertEq(result2, false);
     }
 }
--- test/units/static/JBFeelessAdresses/TestSetFeelessAddress.sol
+++ test/units/static/JBFeelessAdresses/TestSetFeelessAddress.sol
@@ -5,15 +5,23 @@ import /* {*} from */ "../../../helpers/TestBaseWorkflow.sol";
 import {JBFeelessSetup} from "./JBFeelessSetup.sol";
 
 contract TestSetFeelessAddress_Local is JBFeelessSetup {
+    address _feeLess = makeAddr("something");
+
     function setUp() public {
         super.feelessAddressesSetup();
     }
 
     function test_WhenCallerIsOwner() external {
         // it should set isFeeless and emit SetFeelessAddress
+        vm.prank(_owner);
+        _feelessAddresses.setFeelessAddress(_feeLess, true);
+
+        bool result = _feelessAddresses.isFeeless(_feeLess);
+        assertEq(result, true);
     }
 
-    function test_RevertWhen_CallerIsNotOwner() external {
+    function testFail_RevertWhen_CallerIsNotOwner() external {
         // it should revert
+        _feelessAddresses.setFeelessAddress(_feeLess, true);
     }
 }
