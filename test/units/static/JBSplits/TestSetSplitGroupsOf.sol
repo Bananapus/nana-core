b5e468477f3926368e80624a10bebc4928c48873
nowonder <justinpulley92@gmail.com>
2024-01-28 20:15:10 +0000

    setSplitGroupsOf cov

--- test/units/static/JBSplits/TestSetSplitGroupsOf.sol
+++ test/units/static/JBSplits/TestSetSplitGroupsOf.sol
@@ -5,59 +5,400 @@ import /* {*} from */ "../../../helpers/TestBaseWorkflow.sol";
 import {JBSplitsSetup} from "./JBSplitsSetup.sol";
 
 contract TestSetSplitGroupsOf_Local is JBSplitsSetup {
+    address _notThis = makeAddr("notThis");
+    address payable _bene = payable(makeAddr("guy"));
+    uint256 _projectId = 1;
+    uint256 _rulesetId = block.timestamp;
+
     function setUp() public {
         super.splitsSetup();
     }
 
     function test_WhenCallerIsNotController() external {
         // it will revert with CONTROLLER_UNAUTHORIZED
+
+        // data for call
+        JBSplitGroup[] memory _splitsGroup = new JBSplitGroup[](1);
+        JBSplit[] memory _splitsArray = new JBSplit[](1);
+
+        // Set up a payout split recipient.
+        _splitsArray[0] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 2,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: 0,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // outer structure
+        _splitsGroup[0] = JBSplitGroup({
+            groupId: 0,
+            splits: _splitsArray
+        });
+
+        // mock the directory controllerOf call
+        bytes memory _controllerCall = abi.encodeCall(IJBDirectory.controllerOf, (1));
+        bytes memory _return = abi.encode(_notThis);
+        mockExpect(address(directory), _controllerCall, _return);
+
+        // not controller so revert
+        vm.expectRevert(abi.encodeWithSignature("CONTROLLER_UNAUTHORIZED()"));
+        _splits.setSplitGroupsOf(_projectId, _rulesetId, _splitsGroup);
     }
 
     modifier whenCallerIsController() {
+        // mock the directory controllerOf call
+        bytes memory _controllerCall = abi.encodeCall(IJBDirectory.controllerOf, (1));
+        bytes memory _return = abi.encode(address(this));
+        mockExpect(address(directory), _controllerCall, _return);
         _;
     }
 
     function test_GivenPreviouslyLockedSplitsAreNotIncluded() external whenCallerIsController {
         // it will revert with PREVIOUS_LOCKED_SPLITS_NOT_INCLUDED
-    }
 
-    modifier givenPreviouslyLockedSplitsAreIncluded() {
-        _;
+        // data for first call to set locked splits
+        JBSplitGroup[] memory _splitsGroup = new JBSplitGroup[](1);
+        JBSplit[] memory _splitsArray = new JBSplit[](1);
+
+        // Set up a payout split recipient.
+        _splitsArray[0] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 2,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: block.timestamp + 100,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // outer structure
+        _splitsGroup[0] = JBSplitGroup({
+            groupId: 0,
+            splits: _splitsArray
+        });
+
+        _splits.setSplitGroupsOf(_projectId, _rulesetId, _splitsGroup);
+
+        // Re-set up a payout split recipient.
+        _splitsArray[0] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 2,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: 0,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // outer structure
+        _splitsGroup[0] = JBSplitGroup({
+            groupId: 0,
+            splits: _splitsArray
+        });
+
+        vm.expectRevert(abi.encodeWithSignature("PREVIOUS_LOCKED_SPLITS_NOT_INCLUDED()"));
+        _splits.setSplitGroupsOf(_projectId, _rulesetId, _splitsGroup);
     }
 
     function test_GivenAnyConfiguredSplitPercentIsZero()
         external
         whenCallerIsController
-        givenPreviouslyLockedSplitsAreIncluded
     {
         // it will revert with INVALID_SPLIT_PERCENT
+
+        // data for call to set invalid splits
+        JBSplitGroup[] memory _splitsGroup = new JBSplitGroup[](1);
+        JBSplit[] memory _splitsArray = new JBSplit[](1);
+
+        // Set up a payout split recipient.
+        _splitsArray[0] = JBSplit({
+            preferAddToBalance: false,
+            // invalid percent
+            percent: 0,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: 0,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // outer structure
+        _splitsGroup[0] = JBSplitGroup({
+            groupId: 0,
+            splits: _splitsArray
+        });
+
+        vm.expectRevert(abi.encodeWithSignature("INVALID_SPLIT_PERCENT()"));
+        _splits.setSplitGroupsOf(_projectId, _rulesetId, _splitsGroup);
     }
 
-    function test_GivenProjectIdGtUint56Max() external whenCallerIsController givenPreviouslyLockedSplitsAreIncluded {
+    function test_GivenProjectIdGtUint56Max() external whenCallerIsController {
         // it will revert with INVALID_PROJECT_ID
+
+        // data for call with invalid projectId
+        JBSplitGroup[] memory _splitsGroup = new JBSplitGroup[](1);
+        JBSplit[] memory _splitsArray = new JBSplit[](1);
+
+        // Set up a payout split recipient.
+        _splitsArray[0] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 2,
+            // invalid projectId
+            projectId: uint256(type(uint56).max) + 1,
+            beneficiary: _bene,
+            lockedUntil: 0,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // outer structure
+        _splitsGroup[0] = JBSplitGroup({
+            groupId: 0,
+            splits: _splitsArray
+        });
+
+        vm.expectRevert(abi.encodeWithSignature("INVALID_PROJECT_ID()"));
+        _splits.setSplitGroupsOf(_projectId, _rulesetId, _splitsGroup);
     }
 
-    function test_GivenSplitsTotalToOverSPLITS_TOTAL_PERCENT()
+    function test_GivenSplitsTotalToGtSPLITS_TOTAL_PERCENT()
         external
         whenCallerIsController
-        givenPreviouslyLockedSplitsAreIncluded
     {
         // it will revert with INVALID_TOTAL_PERCENT
+
+        // data for call with total percent > 100
+        JBSplitGroup[] memory _splitsGroup = new JBSplitGroup[](1);
+        JBSplit[] memory _splitsArray = new JBSplit[](2);
+
+        // Set up a payout split recipient.
+        _splitsArray[0] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 2,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: 0,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // Set up a second payout split recipient.
+        _splitsArray[1] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: 0,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // outer structure
+        _splitsGroup[0] = JBSplitGroup({
+            groupId: 0,
+            splits: _splitsArray
+        });
+
+        vm.expectRevert(abi.encodeWithSignature("INVALID_TOTAL_PERCENT()"));
+        _splits.setSplitGroupsOf(_projectId, _rulesetId, _splitsGroup);
     }
 
     function test_GivenLockedUntilGtUint48Max()
         external
         whenCallerIsController
-        givenPreviouslyLockedSplitsAreIncluded
     {
         // it will revert with INVALID_LOCKED_UNTIL
+
+        // data for call
+        JBSplitGroup[] memory _splitsGroup = new JBSplitGroup[](1);
+        JBSplit[] memory _splitsArray = new JBSplit[](1);
+
+        // Set up a payout split recipient.
+        _splitsArray[0] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 2,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: uint256(type(uint48).max) + 1,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // outer structure
+        _splitsGroup[0] = JBSplitGroup({
+            groupId: 0,
+            splits: _splitsArray
+        });
+
+        vm.expectRevert(abi.encodeWithSignature("INVALID_LOCKED_UNTIL()"));
+        _splits.setSplitGroupsOf(_projectId, _rulesetId, _splitsGroup);
     }
 
     function test_GivenAllConditionsAreSatisfied()
         external
         whenCallerIsController
-        givenPreviouslyLockedSplitsAreIncluded
     {
         // it will store splits and emit SetSplit for each configured
+
+        // data for call
+        JBSplitGroup[] memory _splitsGroup = new JBSplitGroup[](1);
+        JBSplit[] memory _splitsArray = new JBSplit[](2);
+
+        // Set up a valid payout split recipient.
+        _splitsArray[0] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 2,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: 0,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // Set up a second valid payout split recipient.
+        _splitsArray[1] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 2,
+            projectId: _projectId,
+            beneficiary: payable(makeAddr("anotherBene")),
+            lockedUntil: 0,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // outer structure
+        _splitsGroup[0] = JBSplitGroup({
+            groupId: 0,
+            splits: _splitsArray
+        });
+
+        // should emit an event for each split
+        vm.expectEmit();
+        emit IJBSplits.SetSplit(_projectId, block.timestamp, 0, _splitsArray[0], address(this));
+
+        vm.expectEmit();
+        emit IJBSplits.SetSplit(_projectId, block.timestamp, 0, _splitsArray[1], address(this));
+
+        _splits.setSplitGroupsOf(_projectId, _rulesetId, _splitsGroup);
     }
+
+    function test_GivenSettingExistingIdDomainAndGroup() external whenCallerIsController {
+        // it will overwrite
+
+        // data for call
+        JBSplitGroup[] memory _splitsGroup = new JBSplitGroup[](1);
+        JBSplit[] memory _splitsArray = new JBSplit[](1);
+
+        // Set up a payout split recipient.
+        _splitsArray[0] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 2,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: 0,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // outer structure
+        _splitsGroup[0] = JBSplitGroup({
+            groupId: 0,
+            splits: _splitsArray
+        });
+
+        vm.expectEmit();
+        emit IJBSplits.SetSplit(_projectId, block.timestamp, 0, _splitsArray[0], address(this));
+
+        _splits.setSplitGroupsOf(_projectId, _rulesetId, _splitsGroup);
+
+        // Second or "reconfig" call
+
+        // Re-Set up a payout split recipient.
+        _splitsArray[0] = JBSplit({
+            // Different attributes
+            preferAddToBalance: true,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 3,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: 0,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // outer structure
+        _splitsGroup[0] = JBSplitGroup({
+            groupId: 0,
+            splits: _splitsArray
+        });
+
+        vm.expectEmit();
+        emit IJBSplits.SetSplit(_projectId, block.timestamp, 0, _splitsArray[0], address(this));
+
+        _splits.setSplitGroupsOf(_projectId, block.timestamp, _splitsGroup);
+
+        JBSplit[] memory _current = _splits.splitsOf(_projectId, block.timestamp, 0);
+
+        assertEq(_current[0].preferAddToBalance, true);
+    }
+
+    function test_GivenAddingNewLockedSplitsIncludingPreviouslyLocked() external whenCallerIsController {
+        // it will reconfigure and extend previously locked splits
+
+        // data for call
+        JBSplitGroup[] memory _splitsGroup = new JBSplitGroup[](1);
+        JBSplit[] memory _splitsArray = new JBSplit[](1);
+
+        // Set up a payout split recipient.
+        _splitsArray[0] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 2,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: block.timestamp + 100,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // outer structure
+        _splitsGroup[0] = JBSplitGroup({
+            groupId: 0,
+            splits: _splitsArray
+        });
+
+        vm.expectEmit();
+        emit IJBSplits.SetSplit(_projectId, block.timestamp, 0, _splitsArray[0], address(this));
+
+        _splits.setSplitGroupsOf(_projectId, _rulesetId, _splitsGroup);
+
+        // Second or "reconfig" call
+
+        JBSplit[] memory _splitsArray2 = new JBSplit[](2);
+
+        // Re-Set up a payout split recipient.
+        _splitsArray2[0] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 2,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: block.timestamp + 100,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // Re-Set up a payout split recipient.
+        _splitsArray2[1] = JBSplit({
+            preferAddToBalance: true,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT / 2,
+            projectId: _projectId,
+            beneficiary: _bene,
+            lockedUntil: block.timestamp + 200,
+            hook: IJBSplitHook(address(0))
+        });
+
+        // outer structure
+        _splitsGroup[0] = JBSplitGroup({
+            groupId: 0,
+            splits: _splitsArray2
+        });
+
+        vm.expectEmit();
+        emit IJBSplits.SetSplit(_projectId, block.timestamp, 0, _splitsArray2[0], address(this));
+
+        _splits.setSplitGroupsOf(_projectId, block.timestamp, _splitsGroup);
+
+        JBSplit[] memory _current = _splits.splitsOf(_projectId, block.timestamp, 0);
+
+        assertEq(_current[0].lockedUntil, block.timestamp + 100);
+        assertEq(_current[1].lockedUntil, block.timestamp + 200);
+    }
+
 }
