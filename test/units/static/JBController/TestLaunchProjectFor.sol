37e9228dfac6259acc36ed7b92bb82561734f9ea
nowonder <justinpulley92@gmail.com>
2024-02-01 20:04:41 +0000

    launchProjectFor cov

--- test/units/static/JBController/TestLaunchProjectFor.sol
+++ test/units/static/JBController/TestLaunchProjectFor.sol
@@ -5,6 +5,9 @@ import /* {*} from */ "../../../helpers/TestBaseWorkflow.sol";
 import {JBControllerSetup} from "./JBControllerSetup.sol";
 
 contract TestLaunchProjectFor_Local is JBControllerSetup {
+    using JBRulesetMetadataResolver for JBRulesetMetadata;
+
+    address payable _splitsBeneficiary = payable(makeAddr("someone"));
     string _metadata = "JUICAY_DATA";
     string _memo = "JUICAY_MEMO";
 
@@ -46,7 +49,7 @@ contract TestLaunchProjectFor_Local is JBControllerSetup {
         // it will revert INVALID_RESERVED_RATE()
 
         JBRulesetMetadata memory _rulesMetadata = JBRulesetMetadata({
-            reservedRate: JBConstants.MAX_RESERVED_RATE + 1, // Too damn high
+            reservedRate: JBConstants.MAX_RESERVED_RATE + 1, // invalid
             redemptionRate: JBConstants.MAX_REDEMPTION_RATE / 2, //50%
             baseCurrency: uint32(uint160(JBConstants.NATIVE_TOKEN)),
             pausePay: false,
@@ -87,7 +90,7 @@ contract TestLaunchProjectFor_Local is JBControllerSetup {
 
         JBRulesetMetadata memory _rulesMetadata = JBRulesetMetadata({
             reservedRate: JBConstants.MAX_RESERVED_RATE / 2,
-            redemptionRate: JBConstants.MAX_REDEMPTION_RATE + 1, //50%
+            redemptionRate: JBConstants.MAX_REDEMPTION_RATE + 1, // invalid
             baseCurrency: uint32(uint160(JBConstants.NATIVE_TOKEN)),
             pausePay: false,
             pauseCreditTransfers: false,
@@ -122,9 +125,169 @@ contract TestLaunchProjectFor_Local is JBControllerSetup {
         _controller.launchProjectFor(address(this), _metadata, _rulesetConfigurations, _terminals, _memo);
     }
 
-    /* function test_GivenTerminalsAreProvided() external whenCalled {
+    function test_GivenRulesetHasInvalidBaseCurrency() external whenCalledDefault {
+        // it will revert INVALID_BASE_CURRENCY()
+
+        JBRulesetMetadata memory _rulesMetadata = JBRulesetMetadata({
+            reservedRate: JBConstants.MAX_RESERVED_RATE / 2,
+            redemptionRate: JBConstants.MAX_REDEMPTION_RATE / 2,
+            baseCurrency: uint256(uint256(type(uint32).max) + 1), // invalid
+            pausePay: false,
+            pauseCreditTransfers: false,
+            allowOwnerMinting: false,
+            allowTerminalMigration: false,
+            allowSetTerminals: false,
+            allowControllerMigration: false,
+            allowSetController: false,
+            holdFees: false,
+            useTotalSurplusForRedemptions: true,
+            useDataHookForPay: false,
+            useDataHookForRedeem: false,
+            dataHook: address(0),
+            metadata: 0
+        });
+
+        JBFundAccessLimitGroup[] memory _fundAccessLimitGroup = new JBFundAccessLimitGroup[](0);
+        JBTerminalConfig[] memory _terminals = new JBTerminalConfig[](0);
+
+        // Package up the ruleset configuration.
+        JBRulesetConfig[] memory _rulesetConfigurations = new JBRulesetConfig[](1);
+        _rulesetConfigurations[0].mustStartAtOrAfter = 0;
+        _rulesetConfigurations[0].duration = 0;
+        _rulesetConfigurations[0].weight = 1e18;
+        _rulesetConfigurations[0].decayRate = 0;
+        _rulesetConfigurations[0].approvalHook = IJBRulesetApprovalHook(address(0));
+        _rulesetConfigurations[0].metadata = _rulesMetadata;
+        _rulesetConfigurations[0].splitGroups = new JBSplitGroup[](0);
+        _rulesetConfigurations[0].fundAccessLimitGroups = _fundAccessLimitGroup;
+
+        vm.expectRevert(abi.encodeWithSignature("INVALID_BASE_CURRENCY()"));
+        _controller.launchProjectFor(address(this), _metadata, _rulesetConfigurations, _terminals, _memo);
+    }
+
+    function test_GivenSplitsRulesetsAndFundAccessConstraintsAreConfigured() external whenCalledDefault {
+        // it will set split groups, ruleset, fundAccessConstraints
+
+        JBRulesetMetadata memory _rulesMetadata = JBRulesetMetadata({
+            reservedRate: JBConstants.MAX_RESERVED_RATE / 2,
+            redemptionRate: JBConstants.MAX_REDEMPTION_RATE / 2,
+            baseCurrency: uint32(uint160(JBConstants.NATIVE_TOKEN)),
+            pausePay: false,
+            pauseCreditTransfers: false,
+            allowOwnerMinting: false,
+            allowTerminalMigration: false,
+            allowSetTerminals: false,
+            allowControllerMigration: false,
+            allowSetController: false,
+            holdFees: false,
+            useTotalSurplusForRedemptions: true,
+            useDataHookForPay: false,
+            useDataHookForRedeem: false,
+            dataHook: address(0),
+            metadata: 0
+        });
+
+        uint256 _packed = _rulesMetadata.packRulesetMetadata();
+
+        JBFundAccessLimitGroup[] memory _fundAccessLimitGroup = new JBFundAccessLimitGroup[](0);
+        JBTerminalConfig[] memory _terminals = new JBTerminalConfig[](0);
+
+        // splits
+        JBSplitGroup[] memory _splitsGroup = new JBSplitGroup[](1);
+        JBSplit[] memory _splits = new JBSplit[](1);
+
+        _splits[0] = JBSplit({
+            preferAddToBalance: false,
+            percent: JBConstants.SPLITS_TOTAL_PERCENT,
+            projectId: 1,
+            beneficiary: _splitsBeneficiary,
+            lockedUntil: 0,
+            hook: IJBSplitHook(address(0))
+        });
+
+        _splitsGroup[0] = JBSplitGroup({groupId: uint32(uint160(JBConstants.NATIVE_TOKEN)), splits: _splits});
+
+        // Package up the ruleset configuration.
+        JBRulesetConfig[] memory _rulesetConfigurations = new JBRulesetConfig[](1);
+        _rulesetConfigurations[0].mustStartAtOrAfter = 0;
+        _rulesetConfigurations[0].duration = 0;
+        _rulesetConfigurations[0].weight = 1e18;
+        _rulesetConfigurations[0].decayRate = 0;
+        _rulesetConfigurations[0].approvalHook = IJBRulesetApprovalHook(address(0));
+        _rulesetConfigurations[0].metadata = _rulesMetadata;
+        _rulesetConfigurations[0].splitGroups = _splitsGroup;
+        _rulesetConfigurations[0].fundAccessLimitGroups = _fundAccessLimitGroup;
+
+        // JBRulesets calldata
+        JBRuleset memory returnedRuleset = JBRuleset({
+            cycleNumber: block.timestamp,
+            id: block.timestamp,
+            basedOnId: 0,
+            start: block.timestamp,
+            duration: _rulesetConfigurations[0].duration,
+            weight: _rulesetConfigurations[0].weight,
+            decayRate: _rulesetConfigurations[0].decayRate,
+            approvalHook: _rulesetConfigurations[0].approvalHook,
+            metadata: _packed
+        });
+        bytes memory rulesetsCall = abi.encodeCall(
+            IJBRulesets.queueFor,
+            (
+                1,
+                _rulesetConfigurations[0].duration,
+                _rulesetConfigurations[0].weight,
+                _rulesetConfigurations[0].decayRate,
+                _rulesetConfigurations[0].approvalHook,
+                _packed,
+                _rulesetConfigurations[0].mustStartAtOrAfter
+            )
+        );
+        bytes memory rulesetsReturned = abi.encode(returnedRuleset);
+
+        // JBRulesets call
+        mockExpect(address(rulesets), rulesetsCall, rulesetsReturned);
+
+        // JBSplits call
+        bytes memory splitsCall = abi.encodeCall(IJBSplits.setSplitGroupsOf, (1, block.timestamp, _splitsGroup));
+        bytes memory splitsReturned = abi.encode();
+        mockExpect(address(splits), splitsCall, splitsReturned);
+
+        // JBFundAccess call
+        bytes memory fundsCall =
+            abi.encodeCall(IJBFundAccessLimits.setFundAccessLimitsFor, (1, block.timestamp, _fundAccessLimitGroup));
+        bytes memory fundsReturned = abi.encode();
+        mockExpect(address(fundAccessLimits), fundsCall, fundsReturned);
+
+        _controller.launchProjectFor(address(this), _metadata, _rulesetConfigurations, _terminals, _memo);
+    }
+
+    function test_GivenTerminalsAreProvided() external whenCalledDefault {
         // it will set terminals
-    } */
+        IJBTerminal _terminal = IJBTerminal(makeAddr("terminal"));
+        address _token = makeAddr("token");
+
+        JBRulesetConfig[] memory _rulesets = new JBRulesetConfig[](0);
+        JBTerminalConfig[] memory _terminals = new JBTerminalConfig[](1);
+        address[] memory _tokensAccepted = new address[](1);
+        _tokensAccepted[0] = _token;
+
+        _terminals[0] = JBTerminalConfig({terminal: _terminal, tokensToAccept: _tokensAccepted});
+
+        // mock call return data
+        JBAccountingContext memory _returnedContext =
+            JBAccountingContext({token: _token, decimals: 18, currency: uint32(uint160(JBConstants.NATIVE_TOKEN))});
+
+        // mock call to the terminal addAccountingContextsFor()
+        bytes memory addCall = abi.encodeCall(IJBTerminal.addAccountingContextsFor, (1, _tokensAccepted));
+        bytes memory addReturned = abi.encode(_returnedContext);
+
+        mockExpect(address(_terminal), addCall, addReturned);
+
+        vm.expectEmit();
+        emit IJBController.LaunchProject(0, 1, _metadata, _memo, address(this));
+
+        _controller.launchProjectFor(address(this), _metadata, _rulesets, _terminals, _memo);
+    }
 
     function test_GivenMemoIsProvided() external whenCalledDefault {
         // it will be included in the emit
@@ -137,8 +300,4 @@ contract TestLaunchProjectFor_Local is JBControllerSetup {
 
         _controller.launchProjectFor(address(this), _metadata, _rulesets, _terminals, _memo);
     }
-
-    /* function test_GivenDirectoryExists() external whenCalled {
-        // it will setControllerOf to itself
-    } */
 }
