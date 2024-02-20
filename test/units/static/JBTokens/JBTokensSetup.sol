3199eb9e1d24a167d19f810bb73fe5031593f0e8
nowonder <justinpulley92@gmail.com>
2024-02-20 11:03:06 +0000

    jbtokens cov wip

---
+++ test/units/static/JBTokens/JBTokensSetup.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import /* {*} from */ "../../../helpers/TestBaseWorkflow.sol";
+
+/* 
+Contract that deploys a target contract with other mock contracts to satisfy the constructor.
+Tests relative to this contract will be dependent on mock calls/emits and stdStorage.
+*/
+contract JBTokensSetup is JBTest {
+    // Mocks
+    IJBDirectory public directory = IJBDirectory(makeAddr("directory"));
+
+    // Target Contract
+    IJBTokens public _tokens;
+
+    function terminalStoreSetup() public virtual {
+        // Instantiate the contract being tested
+        _tokens = new JBTokens(directory);
+    }
+}
