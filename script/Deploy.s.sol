73c637c30b4118e778a771992c29c8c1436bc083
0xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2024-03-05 23:58:44 +0100

    feat: finish (basic) sphinx deployment integration

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -21,6 +21,12 @@ contract Deploy is Sphinx {
     /// @notice The universal PERMIT2 address.
     IPermit2 private constant _PERMIT2 = IPermit2(0x000000000022D473030F116dDEE9F6B43aC78BA3);
 
+    /// @notice The address that is allowed to forward calls to the terminal and controller on a users behalf.
+    address private constant TRUSTED_FORWARDER = 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
+
+    /// @notice The address that will manage the few privileged functions of the protocol.
+    address private constant MANAGER = 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
+
     function configureSphinx() public override {
         // TODO: Update to contain JB Emergency Developers
         sphinxConfig.owners = [
@@ -41,24 +47,24 @@ contract Deploy is Sphinx {
         ];
     }
 
+    /// @notice Deploys the protocol.
     function run() sphinx public {
-        address trustedForwarder = 0xB2b5841DBeF766d4b521221732F9B618fCf34A87;
-        address manager = 0x823b92d6a4b2AED4b15675c7917c9f922ea8ADAD;
+        if (
+            block.chainid != 1 &&
+            block.chainid != 11_155_111 &&
+            block.chainid != 420 &&
+            block.chainid != 11_155_420 &&
+            block.chainid != 137 &&
+            block.chainid != 80_001
+        ) {
+            revert("Invalid RPC / no juice contracts deployed on this network");
+        }
 
-        // if (
-        //     block.chainid != 1 &&
-        //     block.chainid != 11_155_111 &&
-        //     block.chainid != 420 &&
-        //     block.chainid != 11_155_420 &&
-        //     block.chainid != 137 &&
-        //     block.chainid != 80_001
-        // ) {
-        //     revert("Invalid RPC / no juice contracts deployed on this network");
-        // }
+        address _sender = safeAddress();
 
         JBPermissions permissions = new JBPermissions();
-        JBProjects projects = new JBProjects(manager);
-        JBDirectory directory = new JBDirectory(permissions, projects, safeAddress());
+        JBProjects projects = new JBProjects(MANAGER);
+        JBDirectory directory = new JBDirectory(permissions, projects, _sender);
         JBSplits splits = new JBSplits(directory);
         JBRulesets rulesets = new JBRulesets(directory);
         directory.setIsAllowedToSetFirstController(
@@ -71,12 +77,12 @@ contract Deploy is Sphinx {
                     tokens: new JBTokens(directory),
                     splits: splits,
                     fundAccessLimits: new JBFundAccessLimits(directory),
-                    trustedForwarder: trustedForwarder
+                    trustedForwarder: TRUSTED_FORWARDER
                 })
             ),
             true
         );
-        directory.transferOwnership(manager);
+        directory.transferOwnership(MANAGER);
         new JBMultiTerminal({
             permissions: permissions,
             projects: projects,
@@ -85,19 +91,11 @@ contract Deploy is Sphinx {
             store: new JBTerminalStore({
                 directory: directory,
                 rulesets: rulesets,
-                prices: new JBPrices(permissions, projects, manager)
+                prices: new JBPrices(permissions, projects, MANAGER)
             }),
-            feelessAddresses: new JBFeelessAddresses(manager),
+            feelessAddresses: new JBFeelessAddresses(MANAGER),
             permit2: _PERMIT2,
-            trustedForwarder: trustedForwarder
+            trustedForwarder: TRUSTED_FORWARDER
         });
-    }
-
-    /// @notice Deploys the protocol.
-    /// @param manager The address that will manage the few privileged functions of the protocol.
-    /// @param trustedForwarder The address that is allowed to forward calls to the terminal and controller on a payer's
-    /// behalf.
-    function _deployJBProtocol(address manager, address trustedForwarder) private {
-        
     }
 }
