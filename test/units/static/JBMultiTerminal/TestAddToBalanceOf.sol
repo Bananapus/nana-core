3cae75838a62a2242f3856fe24609b643b10c4f0
nowonder <justinpulley92@gmail.com>
2024-03-15 01:12:20 +0000

    more addToBalance cases

--- test/units/static/JBMultiTerminal/TestAddToBalanceOf.sol
+++ test/units/static/JBMultiTerminal/TestAddToBalanceOf.sol
@@ -5,6 +5,7 @@ import /* {*} from */ "../../../helpers/TestBaseWorkflow.sol";
 import {JBMultiTerminalSetup} from "./JBMultiTerminalSetup.sol";
 
 contract TestAddToBalanceOf_Local is JBMultiTerminalSetup {
+    // global constants
     uint256 _projectId = 1;
     address _native = JBConstants.NATIVE_TOKEN;
     uint256 _nativeCurrency = uint32(uint160(_native));
@@ -12,13 +13,34 @@ contract TestAddToBalanceOf_Local is JBMultiTerminalSetup {
     uint256 _usdcCurrency = uint32(uint160(_usdc));
     uint256 _terminalUSDCBalance = 1e18;
 
+    // set by modifiers
     uint256 payAmount;
     uint256 feeAmount;
     uint256 feeAmountIn;
+    uint256 amountFromFee;
+    uint256 leftOverAmount;
     bool _shouldReturnHeldFees;
 
     function setUp() public {
         super.multiTerminalSetup();
+    }
+
+    modifier whenNativeTokenIsAccepted() {
+        // Accounting Context to set
+        JBAccountingContext memory _context =
+            JBAccountingContext({token: _native, decimals: 18, currency: uint32(_nativeCurrency)});
+
+        // Find the storage slot
+        bytes32 contextSlot = keccak256(abi.encode(_projectId, uint256(0)));
+        bytes32 slot = keccak256(abi.encode(_native, contextSlot));
+
+        // Set storage
+        vm.store(address(_terminal), slot, bytes32(abi.encode(_context)));
+
+        JBAccountingContext memory _storedContext = _terminal.accountingContextForTokenOf(_projectId, _native);
+        assertEq(_storedContext.token, _native);
+
+        _;
     }
 
     modifier whenShouldReturnHeldFeesEqTrue() {
@@ -55,6 +77,9 @@ contract TestAddToBalanceOf_Local is JBMultiTerminalSetup {
 
         payAmount = 2e18;
         feeAmountIn = JBFees.feeAmountIn(feeAmount, 25);
+
+        amountFromFee = feeAmount - feeAmountIn;
+        leftOverAmount = payAmount - amountFromFee;
 
         _shouldReturnHeldFees = true;
 
@@ -98,9 +123,6 @@ contract TestAddToBalanceOf_Local is JBMultiTerminalSetup {
             abi.encode()
         );
 
-        uint256 amountFromFee = feeAmount - feeAmountIn;
-        uint256 leftOverAmount = payAmount - amountFromFee;
-
         vm.expectEmit();
         emit IJBFeeTerminal.ReturnHeldFees(_projectId, _native, payAmount, feeAmountIn, leftOverAmount, address(this));
 
@@ -123,27 +145,114 @@ contract TestAddToBalanceOf_Local is JBMultiTerminalSetup {
         whenShouldReturnHeldFeesEqTrue
     {
         // it will set heldFeesOf return feeAmountFrom and set leftoverAmount to zero
+        uint256 lowerPayAmount = 1e8;
+        uint256 feeA = JBFees.feeAmountIn(feeAmount, 25);
+        uint256 returnedFee = mulDiv(lowerPayAmount, 25, JBConstants.MAX_FEE);
+
+        // mock call to store recordAddedBalanceFor
+        mockExpect(
+            address(store),
+            abi.encodeCall(IJBTerminalStore.recordAddedBalanceFor, (_projectId, _native, lowerPayAmount + returnedFee)),
+            abi.encode()
+        );
+
+        vm.expectEmit();
+        emit IJBFeeTerminal.ReturnHeldFees(_projectId, _native, lowerPayAmount, returnedFee, 0, address(this));
+
+        _terminal.addToBalanceOf{value: lowerPayAmount}({
+            projectId: _projectId,
+            token: _native,
+            amount: lowerPayAmount,
+            shouldReturnHeldFees: _shouldReturnHeldFees,
+            memo: "",
+            metadata: ""
+        });
+
+        uint256 newFeeAmount = (feeAmount - feeA) - lowerPayAmount;
+
+        // Heldfee should be a new amount
+        JBFee[] memory feesAfter = _terminal.heldFeesOf(_projectId, _native);
+        assertEq(feesAfter[0].amount, newFeeAmount);
     }
 
-    function test_WhenShouldReturnHeldFeesEqFalse() external {
+    function test_WhenShouldReturnHeldFeesEqFalse() external whenNativeTokenIsAccepted {
         // it will call terminalstore recordAddedBalanceFor and emit AddToBalance
+
+        // mock call to store recordAddedBalanceFor
+        mockExpect(
+            address(store),
+            abi.encodeCall(IJBTerminalStore.recordAddedBalanceFor, (_projectId, _native, payAmount + feeAmountIn)),
+            abi.encode()
+        );
+
+        vm.expectEmit();
+        emit IJBTerminal.AddToBalance(_projectId, payAmount, 0, "", "", address(this));
+
+        _terminal.addToBalanceOf{value: payAmount}({
+            projectId: _projectId,
+            token: _native,
+            amount: payAmount,
+            shouldReturnHeldFees: _shouldReturnHeldFees,
+            memo: "",
+            metadata: ""
+        });
     }
 
     function test_WhenTheProjectDNHAccountingContextForTheToken() external {
         // it will revert TOKEN_NOT_ACCEPTED
+
+        vm.expectRevert(abi.encodeWithSignature("TOKEN_NOT_ACCEPTED()"));
+
+        _terminal.addToBalanceOf{value: payAmount}({
+            projectId: _projectId,
+            token: _native,
+            amount: payAmount,
+            shouldReturnHeldFees: _shouldReturnHeldFees,
+            memo: "",
+            metadata: ""
+        });
+
     }
 
-    function test_WhenTheTerminalsTokenEqNativeToken() external {
+    /* function test_WhenTheTerminalsTokenEqNativeToken() external {
         // it will use msg.value
-    }
+        // tested above
+    } */
 
-    function test_WhenTheTerminalsTokenEqNativeTokenAndMsgvalueEqZero() external {
+    function test_WhenTheSpecifiedTokenDNEQNativeTokenAndMsgvalueEqZero() external {
         // it will revert NO_MSG_VALUE_ALLOWED
+
+        // Accounting Context to set
+        JBAccountingContext memory _context =
+            JBAccountingContext({token: _usdc, decimals: 18, currency: uint32(_usdcCurrency)});
+
+        // Find the storage slot
+        bytes32 contextSlot = keccak256(abi.encode(_projectId, uint256(0)));
+        bytes32 slot = keccak256(abi.encode(_usdc, contextSlot));
+
+        // Set storage
+        vm.store(address(_terminal), slot, bytes32(abi.encode(_context)));
+
+        JBAccountingContext memory _storedContext = _terminal.accountingContextForTokenOf(_projectId, _usdc);
+        assertEq(_storedContext.token, _usdc);
+
+        vm.expectRevert(abi.encodeWithSignature("NO_MSG_VALUE_ALLOWED()"));
+
+        _terminal.addToBalanceOf{value: 1}({
+            projectId: _projectId,
+            token: _usdc,
+            amount: payAmount,
+            shouldReturnHeldFees: _shouldReturnHeldFees,
+            memo: "",
+            metadata: ""
+        });
+
     }
 
-    function test_WhenTheTerminalIsCallingItself() external {
+    /* function test_WhenTheTerminalIsCallingItself() external {
         // it will not transfer
-    }
+        // not sure how to test this
+    } */
 
     modifier whenPayMetadataContainsPermitData() {
         _;
