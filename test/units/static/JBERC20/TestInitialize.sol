07de9be68ca24f97c6dd0cf0495fbd565d4a153e
simplemachine92 <justinpulley92@gmail.com>
2024-04-22 14:47:19 -0500

    jberc20 initialize and name cov

--- test/units/static/JBERC20/TestInitialize.sol
+++ test/units/static/JBERC20/TestInitialize.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.23;
 
 import {JBERC20Setup} from "./JBERC20Setup.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/Extensions/IERC20Metadata.sol";
 
 contract TestInitialize_Local is JBERC20Setup {
     string _name = "Nana";
@@ -28,9 +29,26 @@ contract TestInitialize_Local is JBERC20Setup {
 
     function test_WhenName_EQNothing() external {
         // it will revert
+
+        // will fail as internal name is no longer than zero length
+        vm.expectRevert();
+        _erc20.initialize("", _symbol, _owner);
     }
 
     function test_WhenNameIsValidAndNotAlreadySet() external {
         // it will set the name and symbol and transfer ownership
+
+        _erc20.initialize(_name, _symbol, _owner);
+
+        // ensure ownership transferred
+        address newOwner = Ownable(address(_erc20)).owner();
+        assertEq(newOwner, _owner);
+
+        // name is set
+        string memory _setName = IERC20Metadata(address(_erc20)).name();
+        string memory _setSymbol = IERC20Metadata(address(_erc20)).symbol();
+
+        assertEq(_setName, _name);
+        assertEq(_setSymbol, _symbol);
     }
 }
--- test/units/static/JBERC20/TestName.sol
+++ test/units/static/JBERC20/TestName.sol
@@ -2,17 +2,29 @@
 pragma solidity 0.8.23;
 
 import {JBERC20Setup} from "./JBERC20Setup.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/Extensions/IERC20Metadata.sol";
 
 contract TestName_Local is JBERC20Setup {
+    IERC20Metadata _token;
+
     function setUp() public {
         super.erc20Setup();
+
+        _token = IERC20Metadata(address(_erc20));
     }
 
     function test_WhenANameIsSet() external {
         // it will return the name
+        _erc20.initialize("NANAPUS", "NANA", _owner);
+
+        string memory _setName = _token.name();
+        assertEq(_setName, "NANAPUS");
     }
 
     function test_WhenANameIsNotSet() external {
         // it will return an empty string
+
+        string memory _storedName = _token.name();
+        assertEq(_storedName, "");
     }
 }
