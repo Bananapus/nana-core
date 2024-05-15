1668fa5b6bf8e7705871a10c39686fa71de185a6
0xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2024-05-15 17:40:42 +0200

    test: add another more extensive test for C-02

--- test/TestJBERC20Inheritance.sol
+++ test/TestJBERC20Inheritance.sol
@@ -14,4 +14,70 @@ contract JBERC20Inheritance_Local is JBERC20, TestBaseWorkflow {
 
         _update(address(0), address(100), _max + 1);
     }
+    
+    // This test checks that voting power gets accounted for.
+    function test_votesUpdateOnTransfer() public {
+        JBRulesetMetadata memory _metadata = JBRulesetMetadata({
+            reservedRate: 0,
+            redemptionRate: 0,
+            baseCurrency: uint32(uint160(JBConstants.NATIVE_TOKEN)),
+            pausePay: false,
+            pauseCreditTransfers: false,
+            allowOwnerMinting: true,
+            allowTerminalMigration: false,
+            allowSetTerminals: false,
+            allowControllerMigration: false,
+            allowSetController: false,
+            holdFees: false,
+            useTotalSurplusForRedemptions: false,
+            useDataHookForPay: true,
+            useDataHookForRedeem: true,
+            dataHook: address(0),
+            metadata: 0
+        });
+        address _projectOwner = multisig();
+        address _recipient = address(200);
+        uint256 _amount = 10 ether;
+
+        // Package up ruleset configuration.
+        JBRulesetConfig[] memory _rulesetConfig = new JBRulesetConfig[](1);
+        _rulesetConfig[0].mustStartAtOrAfter = 0;
+        _rulesetConfig[0].duration = 0;
+        _rulesetConfig[0].weight = 0;
+        _rulesetConfig[0].decayRate = 0;
+        _rulesetConfig[0].approvalHook = IJBRulesetApprovalHook(address(0));
+        _rulesetConfig[0].metadata = _metadata;
+        _rulesetConfig[0].splitGroups = new JBSplitGroup[](0);
+        _rulesetConfig[0].fundAccessLimitGroups = new JBFundAccessLimitGroup[](0);
+
+        // Package up terminal configuration.
+        JBTerminalConfig[] memory _terminalConfigurations = new JBTerminalConfig[](1);
+        address[] memory _tokensToAccept = new address[](1);
+        _tokensToAccept[0] = JBConstants.NATIVE_TOKEN;
+        _terminalConfigurations[0] = JBTerminalConfig({terminal: jbMultiTerminal(), tokensToAccept: _tokensToAccept});
+
+        uint256 projectId = jbController().launchProjectFor({
+            owner: _projectOwner,
+            projectUri: "myIPFSHash",
+            rulesetConfigurations: _rulesetConfig,
+            terminalConfigurations: _terminalConfigurations,
+            memo: ""
+        });
+        
+        // Configure a token.
+        vm.prank(_projectOwner);
+        IJBToken _token = jbController().deployERC20For(projectId, "TestToken", "TEST", bytes32(0));
+        
+        // Have the user delegate to themselves.
+        vm.prank(_recipient);
+        ERC20Votes(address(_token)).delegate(_recipient);
+
+        // Mint tokens to the user, check that the balance is correct. 
+        vm.startPrank(_projectOwner);
+        jbController().mintTokensOf(projectId, _amount, _recipient, "", false);  
+        assertEq(_token.balanceOf(_recipient), _amount);
+        
+        // Assert that the user received the voting power as expected.
+        assertEq(ERC20Votes(address(_token)).getVotes(_recipient), _amount);
+    }
 }
