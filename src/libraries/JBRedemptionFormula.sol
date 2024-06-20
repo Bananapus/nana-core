bbe9f86926a74a975392dc1f895f5048cd05d9d9
Filip <f@filip.world>
2024-06-20 16:02:00 -0400

    natspec updates

--- src/libraries/JBRedemptionFormula.sol
+++ src/libraries/JBRedemptionFormula.sol
@@ -6,18 +6,17 @@ import {mulDiv} from "@prb/math/src/Common.sol";
 
 import {JBConstants} from "./JBConstants.sol";
 
-library JBRedemptionFormula {
-    /// @notice The amount of surplus which is available for reclaiming via redemption given the number of tokens being
-    /// redeemed, the total supply, the current surplus, and the current ruleset.
-    /// @param surplus The surplus amount to make the calculation with.
-    /// @param tokenCount The number of tokens to make the calculation with, as a fixed point number with 18 decimals.
-    /// @param totalSupply The total supply of tokens to make the calculation with, as a fixed point number with 18
-    /// decimals.
-    /// @param redemptionRate The redemption rate with which the reclaimable surplus is being calculated.
+/// @notice Redemption calculations.
+library JBRedemptions {
+    /// @notice Returns the amount of surplus terminal tokens which can be reclaimed based on the total surplus, the number of tokens being redeemed, the total token supply, and the ruleset's redemption rate.
+    /// @param surplus The total amount of surplus terminal tokens.
+    /// @param tokensRedeemed The number of tokens being redeemed, as a fixed point number with 18 decimals.
+    /// @param totalSupply The total token supply, as a fixed point number with 18 decimals.
+    /// @param redemptionRate The current ruleset's redemption rate.
     /// @return The amount of surplus tokens that can be reclaimed.
-    function reclaimableSurplusFrom(
+    function reclaimFrom(
         uint256 surplus,
-        uint256 tokenCount,
+        uint256 tokensRedeemed,
         uint256 totalSupply,
         uint256 redemptionRate
     )
@@ -25,24 +24,24 @@ library JBRedemptionFormula {
         pure
         returns (uint256)
     {
-        // If the redemption rate is 0, nothing is claimable.
+        // If the redemption rate is 0, no surplus can be reclaimed.
         if (redemptionRate == 0) return 0;
 
-        // If the amount being redeemed is the total supply, return the rest of the surplus.
-        if (tokenCount == totalSupply) return surplus;
+        // If the total supply is being redeemed, return the entire surplus.
+        if (tokensRedeemed == totalSupply) return surplus;
 
         // Get a reference to the linear proportion.
-        uint256 base = mulDiv(surplus, tokenCount, totalSupply);
+        uint256 base = mulDiv(surplus, tokensRedeemed, totalSupply);
 
-        // These conditions are all part of the same curve. Edge conditions are separated because fewer operation are
-        // necessary.
+        // These conditions are all part of the same curve.
+        // Edge conditions are separated to minimize the operations performed in those cases.
         if (redemptionRate == JBConstants.MAX_REDEMPTION_RATE) {
             return base;
         }
 
         return mulDiv(
             base,
-            redemptionRate + mulDiv(tokenCount, JBConstants.MAX_REDEMPTION_RATE - redemptionRate, totalSupply),
+            redemptionRate + mulDiv(tokensRedeemed, JBConstants.MAX_REDEMPTION_RATE - redemptionRate, totalSupply),
             JBConstants.MAX_REDEMPTION_RATE
         );
     }
