979dd98495202a5331e2564d68fc8c97269912e8
simplemachine92 <justinpulley92@gmail.com>
2024-09-24 19:26:29 -0500

    JBMT cov very near 100%

--- test/units/static/JBMultiTerminal/TestSendPayoutsOf.sol
+++ test/units/static/JBMultiTerminal/TestSendPayoutsOf.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.23;
 import /* {*} from */ "../../../helpers/TestBaseWorkflow.sol";
 import {JBMultiTerminalSetup} from "./JBMultiTerminalSetup.sol";
 
-contract TestPayoutsOf_Local is JBMultiTerminalSetup {
+contract TestSendPayoutsOf_Local is JBMultiTerminalSetup {
     uint256 _projectId = 1;
     uint256 _defaultAmount = 1e18;
 
@@ -61,74 +61,110 @@ contract TestPayoutsOf_Local is JBMultiTerminalSetup {
         _terminal.sendPayoutsOf(_projectId, address(0), 0, 0, 1);
     }
 
-    modifier whenASplitHookIsConfigured() {
-        _;
+    function test_WhenOwnerMustSendPayoutsButCallerDNEQOwner() external {
+        // it will check permissions
+
+        // needed for terminal store mock call
+        JBRuleset memory returnedRuleset = generateUnfriendlyRuleset();
+
+        JBAccountingContext memory mockTokenContext = JBAccountingContext({token: address(0), decimals: 0, currency: 0});
+
+        // record payout mock call
+        mockExpect(
+            address(store),
+            abi.encodeCall(IJBTerminalStore.recordPayoutFor, (_projectId, mockTokenContext, 0, 0)),
+            abi.encode(returnedRuleset, 0)
+        );
+
+        address owner = makeAddr("owner");
+
+        // projects owner of
+        mockExpect(address(projects), abi.encodeCall(IERC721.ownerOf, (_projectId)), abi.encode(owner));
+
+        // mock permissions call
+        bytes memory _permCall = abi.encodeCall(
+            IJBPermissions.hasPermission, (address(this), owner, _projectId, JBPermissionIds.SEND_PAYOUTS, true, true)
+        );
+        mockExpect(address(permissions), _permCall, abi.encode(false));
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                JBPermissioned.JBPermissioned_Unauthorized.selector,
+                owner,
+                address(this),
+                _projectId,
+                JBPermissionIds.SEND_PAYOUTS
+            )
+        );
+        _terminal.sendPayoutsOf(_projectId, address(0), 0, 0, 1);
     }
 
-    function test_GivenTheSplitHookIsFeeless() external whenASplitHookIsConfigured {
-        // it will not process a fee
-    }
+    function test_WhenExecutePayoutFails() external {
+        // it will revert PayoutReverted
 
-    function test_GivenTheSplitHookDNEQFeeless() external whenASplitHookIsConfigured {
-        // it will process a fee
-    }
+        // needed for terminal store mock call
+        JBRuleset memory returnedRuleset = JBRuleset({
+            cycleNumber: 1,
+            id: 1,
+            basedOnId: 0,
+            start: 0,
+            duration: 0,
+            weight: 0,
+            decayPercent: 0,
+            approvalHook: IJBRulesetApprovalHook(address(0)),
+            metadata: 0
+        });
 
-    function test_GivenTheSplitHookDNSupportSplitHookInterface() external whenASplitHookIsConfigured {
-        // it will revert 400_1
-    }
+        JBAccountingContext memory mockTokenContext = JBAccountingContext({token: address(0), decimals: 0, currency: 0});
 
-    function test_GivenThePayoutTokenIsErc20() external whenASplitHookIsConfigured {
-        // it will safe increase allowance
-    }
+        // record payout mock call
+        mockExpect(
+            address(store),
+            abi.encodeCall(IJBTerminalStore.recordPayoutFor, (_projectId, mockTokenContext, 0, 0)),
+            abi.encode(returnedRuleset, 0)
+        );
 
-    function test_GivenThePayoutTokenIsNative() external whenASplitHookIsConfigured {
-        // it will send eth in msgvalue
-    }
+        // projects owner of
+        mockExpect(address(projects), abi.encodeCall(IERC721.ownerOf, (_projectId)), abi.encode(address(this)));
 
-    modifier whenASplitProjectIdIsConfigured() {
-        _;
-    }
+        // needed for splits return call
+        // invalid data to ensure revert
+        JBSplit[] memory returnedSplits = new JBSplit[](1);
+        returnedSplits[0] = JBSplit({
+            preferAddToBalance: false,
+            percent: 0,
+            projectId: 0,
+            beneficiary: payable(address(this)),
+            lockedUntil: uint48(block.timestamp + 1),
+            hook: IJBSplitHook(address(0))
+        });
 
-    function test_GivenTheProjectsTerminalEQZeroAddress() external whenASplitProjectIdIsConfigured {
-        // it will revert 404_2
-    }
+        // mock splits of call
+        mockExpect(
+            address(splits),
+            abi.encodeCall(IJBSplits.splitsOf, (_projectId, returnedRuleset.id, 0)),
+            abi.encode(returnedSplits)
+        );
 
-    function test_GivenPreferAddToBalanceEQTrueAndTerminalEQThisAddress() external whenASplitProjectIdIsConfigured {
-        // it will call _addToBalanceOf internal
-    }
+        // mock directory call for fee processing
+        mockExpect(
+            address(directory),
+            abi.encodeCall(IJBDirectory.primaryTerminalOf, (1, address(0))),
+            abi.encode(address(_terminal))
+        );
 
-    function test_GivenPreferAddToBalanceEQTrueAndTerminalEQAnotherAddress() external whenASplitProjectIdIsConfigured {
-        // it will call that terminals addToBalanceOf
-    }
+        // mock call to feelessAddresses
+        mockExpect(
+            address(feelessAddresses), abi.encodeCall(IJBFeelessAddresses.isFeeless, (address(this))), abi.encode(true)
+        );
 
-    function test_GivenPreferAddToBalanceDNEQTrueAndTerminalEQThisAddress() external whenASplitProjectIdIsConfigured {
-        // it will call internal _pay
-    }
-
-    function test_GivenPreferAddToBalanceDNEQTrueAndTerminalEQAnotherAddress()
-        external
-        whenASplitProjectIdIsConfigured
-    {
-        // it will call that terminals pay function
-    }
-
-    modifier whenABeneficiaryIsConfigured() {
-        _;
-    }
-
-    function test_GivenBeneficiaryEQFeeless() external whenABeneficiaryIsConfigured {
-        // it will payout to the beneficiary without taking fees
-    }
-
-    function test_GivenBeneficiaryDNEQFeeless() external whenABeneficiaryIsConfigured {
-        // it will payout to the beneficiary incurring fee
-    }
-
-    function test_WhenThereIsNoBeneficiarySplitHookOrProjectToPay() external {
-        // it will payout msgSender
-    }
-
-    function test_WhenThereAreLeftoverPayoutFunds() external {
-        // it will payout the rest to the project owner
+        // this expectEmit doesn't work as intended, I suppose because of the catch block which we are testing.
+        // nonetheless we trigger the intended code.
+        /*
+        vm.expectEmit(address(0x5615dEB798BB3E4dFa0139dFa1b3D433Cc23b72f));
+        emit IJBPayoutTerminal.PayoutReverted(_projectId, returnedSplits[0], 0,
+        bytes("0x9996b3150000000000000000000000000000000000000000000000000000000000000000"), address(this)); 
+        */
+        _terminal.sendPayoutsOf(_projectId, address(0), 0, 0, 0);
     }
 }
