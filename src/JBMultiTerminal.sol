a8bc8df5bb9dd7453d91145a0ac96b0b945d1068
mejango <mejango@protonmail.com>
2024-11-14 16:38:03 -0300

    fmt

--- src/JBMultiTerminal.sol
+++ src/JBMultiTerminal.sol
@@ -686,11 +686,8 @@ contract JBMultiTerminal is JBPermissioned, ERC2771Context, IJBMultiTerminal {
         uint256 beneficiaryBalanceAfter = tokens.totalBalanceOf({holder: beneficiary, projectId: projectId});
 
         // Set the beneficiary token count.
-        if (beneficiaryBalanceAfter <= beneficiaryBalanceBefore) {
-            beneficiaryTokenCount = 0;
-        } else {
-            beneficiaryTokenCount = beneficiaryBalanceAfter - beneficiaryBalanceBefore;
-        }
+        beneficiaryTokenCount =
+            beneficiaryBalanceAfter <= beneficiaryBalanceBefore ? 0 : beneficiaryBalanceAfter - beneficiaryBalanceBefore;
 
         // The token count for the beneficiary must be greater than or equal to the specified minimum.
         if (beneficiaryTokenCount < minReturnedTokens) {
