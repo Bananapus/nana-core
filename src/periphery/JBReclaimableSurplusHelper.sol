648d7a03a6ab3df927198560d6bb1ac62d422127
0xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2024-12-01 22:24:31 +0100

    feat: added reclaimable

--- src/periphery/JBReclaimableSurplusHelper.sol
+++ src/periphery/JBReclaimableSurplusHelper.sol
@@ -2,13 +2,28 @@
 pragma solidity 0.8.23;
 
 import {JBSurplus} from "../libraries/JBSurplus.sol";
+import {JBCashOuts} from "../libraries/JBCashOuts.sol";
+import {JBRulesetMetadataResolver} from "../libraries/JBRulesetMetadataResolver.sol";
+
 import {IJBDirectory} from "../interfaces/IJBDirectory.sol";
+import {IJBController} from "../interfaces/IJBController.sol";
+import {IJBRulesets} from "../interfaces/IJBRulesets.sol";
+import {JBRuleset} from "../structs/JBRuleset.sol";
 
 /// @notice a UI helper contract for getting the current total reclaimable surplus of a project.
 contract JBReclaimableSurplusHelper {
-    IJBDirectory public immutable DIRECTORY;
+    // A library that parses the packed ruleset metadata into a friendlier format.
+    using JBRulesetMetadataResolver for JBRuleset;
 
-    function currentTotalReclaimableSurplusOf(
+    IJBDirectory public immutable DIRECTORY;
+    IJBRulesets public immutable RULESETS;
+
+    constructor(IJBDirectory directory, IJBRulesets rulesets) {
+        DIRECTORY = directory;
+        RULESETS = rulesets;
+    }
+
+    function currentTotalSurplusOf(
         uint256 projectId,
         uint256 decimals,
         uint256 currency
@@ -18,5 +33,40 @@ contract JBReclaimableSurplusHelper {
         returns (uint256 surplus)
     {
         return JBSurplus.currentSurplusOf(projectId, DIRECTORY.terminalsOf(projectId), decimals, currency);
+    }
+
+    function currentTotalReclaimableSurplusOf(
+        uint256 projectId,
+        uint256 cashOutCount,
+        uint256 decimals,
+        uint256 currency
+    )
+        external
+        view
+        returns (uint256 surplus)
+    {
+        // Get a reference to the project's current ruleset.
+        JBRuleset memory ruleset = RULESETS.currentOf(projectId);
+
+        uint256 currentSurplus =
+            JBSurplus.currentSurplusOf(projectId, DIRECTORY.terminalsOf(projectId), decimals, currency);
+
+        // If there's no surplus, nothing can be reclaimed.
+        if (currentSurplus == 0) return 0;
+
+        // Get the project token's total supply.
+        uint256 totalSupply =
+            IJBController(address(DIRECTORY.controllerOf(projectId))).totalTokenSupplyWithReservedTokensOf(projectId);
+
+        // Can't cash out more tokens than are in the total supply.
+        if (cashOutCount > totalSupply) return 0;
+
+        // Return the amount of surplus terminal tokens that would be reclaimed.
+        return JBCashOuts.cashOutFrom({
+            surplus: currentSurplus,
+            cashOutCount: cashOutCount,
+            totalSupply: totalSupply,
+            cashOutTaxRate: ruleset.cashOutTaxRate()
+        });
     }
 }
