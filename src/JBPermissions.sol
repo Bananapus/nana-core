374dc8e09ff7d3fcd95408021fb3a64a01de830c
mejango <mejango@protonmail.com>
2025-01-06 11:05:41 -0300

    spacing

--- src/JBPermissions.sol
+++ src/JBPermissions.sol
@@ -226,17 +226,13 @@ contract JBPermissions is IJBPermissions {
                             includeWildcardProjectId: true
                         })
                 )
-        ) {
-            revert JBPermissions_Unauthorized();
-        }
+        ) revert JBPermissions_Unauthorized();
 
         // ROOT permission cannot be set for a wildcard project ID.
         if (
             permissionsData.projectId == WILDCARD_PROJECT_ID
                 && _includesPermission({permissions: packed, permissionId: JBPermissionIds.ROOT})
-        ) {
-            revert JBPermissions_CantSetRootPermissionForWildcardProject();
-        }
+        ) revert JBPermissions_CantSetRootPermissionForWildcardProject();
 
         // Store the new value.
         permissionsOf[permissionsData.operator][account][permissionsData.projectId] = packed;
