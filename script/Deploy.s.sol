de8e6e014e3f6a702b0b93b21ace1e9758306e8e
mejango <mejango@protonmail.com>
2025-02-17 00:14:53 -0300

    artifacts

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -38,7 +38,7 @@ contract Deploy is Script, Sphinx {
 
     /// @notice The nonce that gets used across all chains to sync deployment addresses and allow for new deployments of
     /// the same bytecode.
-    uint256 private CORE_DEPLOYMENT_NONCE = 18;
+    uint256 private CORE_DEPLOYMENT_NONCE = 1;
 
     function configureSphinx() public override {
         // TODO: Update to contain JB Emergency Developers
